c45f338ca6c81317bc5d1191c5b7b0a8ba837b95
brandon-tay <brandon.tay@one-line.com>
2021-09-16 08:21:45 +0000

    Fixing to a single text param for detect intent

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def helloWorld():
 
 
 # [START dialogflow_detect_intent_text]
-@app.route('/detect/intent/texts/<texts>')
-def detect_intent_texts(project_id='one-ghq-gcp-study', session_id='fb2e77d47a047kk00504cb3ab4a1f626d174d2d', texts='', language_code='en'):
+@app.route('/detect/intent/texts/<project_id>/<session_id>/<text>/<language_code>')
+def detect_intent_texts(project_id='one-ghq-gcp-study', session_id='fb2e77d47a047kk00504cb3ab4a1f626d174d2d', text='', language_code='en'):
     """Returns the result of detect intent with texts as inputs.
     Using the same `session_id` between requests allows continuation
     of the conversation."""
@@ -25,16 +25,17 @@ def detect_intent_texts(project_id='one-ghq-gcp-study', session_id='fb2e77d47a04
 
     fulfillment_text = ''
 
-    for text in texts:
-        text_input = dialogflow.TextInput(text=text, language_code=language_code)
-        query_input = dialogflow.QueryInput(text=text_input)
-        response = session_client.detect_intent(request={"session": session, "query_input": query_input})
+    #for text in texts:
+    
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(request={"session": session, "query_input": query_input})
 
-        fulfillment_text = response.query_result.fulfillment_text
+    fulfillment_text = response.query_result.fulfillment_text
 
-        logging.warning("Query text: {}".format(response.query_result.query_text))
-        logging.warning("Detected intent: {} (confidence: {})\n".format(response.query_result.intent.display_name, response.query_result.intent_detection_confidence))
-        logging.warning("Fulfillment text: {}\n".format(fulfillment_text))
+    logging.warning("Query text: {}".format(response.query_result.query_text))
+    logging.warning("Detected intent: {} (confidence: {})\n".format(response.query_result.intent.display_name, response.query_result.intent_detection_confidence))
+    logging.warning("Fulfillment text: {}\n".format(fulfillment_text))
 
     return fulfillment_text
 
