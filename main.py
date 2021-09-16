96cbee735126cbf72406095d892e5d04780f82b0
brandon-tay <brandon.tay@one-line.com>
2021-09-16 06:26:28 +0000

    Return fulfillment_text for detect intent texts

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ def detect_intent_texts(project_id='', session_id='', texts='', language_code=''
     session = session_client.session_path(project_id, session_id)
     logging.warning("Session path: {}\n".format(session))
 
+    fulfillment_text = ''
+
     for text in texts:
         text_input = dialogflow.TextInput(text=text, language_code=language_code)
 
@@ -30,9 +32,13 @@ def detect_intent_texts(project_id='', session_id='', texts='', language_code=''
 
         response = session_client.detect_intent(request={"session": session, "query_input": query_input})
 
+        fulfillment_text = response.query_result.fulfillment_text
+
         logging.warning("Query text: {}".format(response.query_result.query_text))
         logging.warning("Detected intent: {} (confidence: {})\n".format(response.query_result.intent.display_name, response.query_result.intent_detection_confidence))
-        logging.warning("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+        logging.warning("Fulfillment text: {}\n".format(fulfillment_text))
+
+    return fulfillment_text
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
